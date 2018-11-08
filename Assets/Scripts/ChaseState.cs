c89529ea03a179383f72008265b5e10a1ef48026
Rickbieze <rickbieze@hotmail.com>
2018-11-08 20:47:16 +0100

    ai field of view attempt
    not finished.

--- Assets/Scripts/ChaseState.cs
+++ Assets/Scripts/ChaseState.cs
@@ -1,13 +1,30 @@
-﻿using UnityEngine;using System.Collections;using UnityEngine.AI;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
 
-public class ChaseState : FSMState{    public ChaseState()
-    {        stateID = FSMStateID.Chasing;    }    public override void Reason(Transform player, Transform npc)    {
-        NavMeshAgent agent = npc.GetComponent<NavMeshAgent>();
-        //Check the distance with player tank
-        //When the distance is near, transition to chase state
-        if (Vector3.Distance(npc.position, player.position) >= 5.0f)        {            Debug.Log("Lost Player");            npc.GetComponent<BleddynController>().SetTransition(Transition.LostPlayer);        }    }    public override void Act(Transform player, Transform npc)    {
+public class ChaseState : FSMState
+{
+    public ChaseState()
+    {
+        stateID = FSMStateID.Chasing;
+    }
+
+    public override void Reason(Transform player, Transform npc)
+    {
+        //check the distance between player and enemy
+        //when the distance is greather than 5, the enemy lost the player
+        if (Vector3.Distance(npc.position, player.position) >= 5.0f)
+        {
+            Debug.Log("Lost Player");
+            npc.GetComponent<BleddynController>().SetTransition(Transition.LostPlayer);
+        }
+    }
+
+    public override void Act(Transform player, Transform npc)
+    {
         NavMeshAgent agent = npc.GetComponent<NavMeshAgent>();
         agent.destination = player.position;
         var distance = Vector3.Distance(npc.position, player.position);
         Debug.Log("chasing, Distance: " + distance);
-    }}
+    }
+}
